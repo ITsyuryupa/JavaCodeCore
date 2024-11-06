0edfb750e42345bcb541dfb897f2d553947e361f
ITsyuryupa <openingthedoor@yandex.ru>
2024-11-06 10:34:28 +0300

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import MyStringBuilder.MyStringBuilder;
+import MyStringBuilder.MementoStorage;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuilder s = new StringBuilder();
+
+        MyStringBuilder sb = new MyStringBuilder();
+        MementoStorage m = new MementoStorage(sb);
+
+        sb.append("Hello, ");
+        char[] worldArray = {'w', 'o', 'r', 'l', 'd', '!'};
+        sb.append(worldArray);
+        System.out.println(sb);
+
+
+        m.save();
+
+        sb.delete(1,2);
+        System.out.println(sb);
+
+        sb.replace(2,3,"q");
+        System.out.println(sb);
+
+        sb.replace(2,2,"h");
+        System.out.println(sb);
+        m.undo();
+        System.out.println(sb);
+
+
     }
 }
